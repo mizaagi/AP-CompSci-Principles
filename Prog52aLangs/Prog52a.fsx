d360b9ecd2ec293eec7a73099d1b571541f9f46c
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-09-05 15:33:39 +0000

    9/5/2024

--- Prog52aLangs/Prog52a.fsx
+++ Prog52aLangs/Prog52a.fsx
@@ -1,8 +1,11 @@
-let length = 0
-let width = 0
-let area = 0
-let perim = 0
+open System
 
 
+Console.WriteLine("Enter length: ")
+let length = int(Console.ReadLine())
+Console.WriteLine("Enter width: ")
+let width = int(Console.ReadLine())
+let area = length * width
+let perim = 2*length + 2*width
 
 Console.WriteLine($"Area: {area}\nPerim: {perim}")
---
+++ Prog52aLangs/Prog52a.swift
@@ -0,0 +1,6 @@
+
+print("Enter length: "); int length = int(readLine())
+print("Enter width: "); int width = int(readLine())
+
+int area = length * width
+int perim = 2*lengh + 2*width
