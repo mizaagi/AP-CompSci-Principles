6cdf173d9f786d3599760fb63fbbd92fc5343da7
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-09-05 13:42:51 +0000

    Edits to C code

--- Prog52aLangs/Prog52a.c
+++ Prog52aLangs/Prog52a.c
@@ -3,18 +3,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main() {
-    int length;
+void main() {
+    int length = 0;
     printf("Enter the length of your rectangle: \n");
-    scanf("%d", &length);
-    int width;
+    // TODO
+    int width = 0;
     printf("Enter the width of your rectangle: \n");
-    scanf("%d", &width);
-    int area;
-    int perim;
+    // TODO
+    int area = 0;
+    int perim = 0;
     area = length * width;
     perim = 2*length + 2*width;
     printf("Area: %d\n", area);
     printf("Perim: %d\n", perim);
-    return 0;
 }
