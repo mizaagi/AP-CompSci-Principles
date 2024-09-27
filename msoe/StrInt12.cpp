19df7d1eb5babca70c9a39ff8c41a30070e644cd
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-09-27 16:22:56 +0000

    UNFINISHED STRINT12

--- msoe/StrInt12.cpp
+++ msoe/StrInt12.cpp
@@ -3,7 +3,27 @@
 #include <string>
 #include <math.h>
 using namespace std;
-
-int main() {
+/* int main() {
     cout << "Enter a string";
+    int len = 0;
+    string s;
+    cin >> s;
+    int currentWordNum = 0;
+    for (lcv = 0; lcv < s.length(); lcv++) {
+
+    }
+    char words[20][10];
+    char currentWord[20];
+    int listLength = 0;
+    for (int lcv = 0; lcv < s.length(); lcv++) {
+        if (s[lcv] == " ") {
+            words[currentWordNum] = currentWord;
+            currentWord = ""
+            currentWordNum++;
+        } else {
+            currentWord = currentWord + s[lcv];
+        }
+    }
+    for ()
 }
+*/
