c2dcff4bd2b19cc39593978ac087fdf0e4fcb174
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-10-02 19:29:01 +0000

    I DID STRINT12!!!!

--- msoe/StrInt12.cpp
+++ msoe/StrInt12.cpp
@@ -7,29 +7,43 @@
 using namespace std;
 
 int main() {
-    cout << "Enter a string: ";
+    // Declare everything
     int len = 0;
-    string s;
-    cin >> s;
     int currentWordNum = 0;
     vector<string> words {};
     string currentWord;
     int listLength = 0;
+    string s;
+
+    // Get string to analyze
+    cout << "Enter a string: ";
+    getline(cin, s);
+    cout << "length: " << s.length() << endl;
+
+    // Figuring out what words there are, adding them to a vector
     for (int lcv = 0; lcv < s.length(); lcv++) {
         if (s[lcv] ==  ' ') {
+            cout << "Word added: " << currentWord << endl;
             words.push_back(currentWord);
             currentWord = "";
             currentWordNum++;
         } else {
             currentWord = currentWord + s[lcv];
+            cout << "Building word... " << currentWord << endl;
         }
     }
+    cout << "Word added: " << currentWord << endl;
+            words.push_back(currentWord);
+            currentWord = "";
+            currentWordNum++;
     string allWords;
-    cout << words.size();
-    for (int lcv2 = 0; lcv2 < words.size(); lcv2++) {
+    
+    // Output
+    cout << "Number of words: " << words.size() << endl;
+    for (int lcv2 = words.size()-1; lcv2 >= 0; lcv2--) {
         allWords += words[lcv2];
         allWords += ' ';
     }
-    cout << allWords;
+    cout << "All words, reversed: " << allWords;
 }
 
