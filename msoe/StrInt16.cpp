b3aa6c06148f818dc9d6f3e678742707789e05ba
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-10-09 22:55:05 +0000

    10/9/2024

--- msoe/StrInt16.cpp
+++ msoe/StrInt16.cpp
@@ -2,6 +2,24 @@
 #include <string>
 using namespace std;
 
-int main() { 
+int main() {
+    string source;
+    string target;
+    int targetCount = 0;
+    cout << "Enter source string: ";
+    cin >> source;
+    cout << endl << "Enter target string: ";
+    cin >> target;
 
+    for (int lcv = 0; lcv <= source.length() - target.length(); lcv++) {
+        if (source.substr(lcv, target.length()).compare(target) == 0) {
+            targetCount++;
+        }
+    }
+
+    if (targetCount > 0) {
+        cout << "The string " << target << " is in the string " << source << "." << endl;
+    } else {
+        cout << "The string " << target << " is not in the string " << source << "." << endl;
+    }
 }
--- msoe/msoe2012-9.cpp
+++ msoe/msoe2012-9.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <vector>
 using namespace std;
 
+/*
+The plan is to tackle this problem with vectors. I am going to iterate through the string of numerals
+that the user inputs and then convert them to a list of numbers. Then, we can compare which of the numbers
+are greater or lesser and figure out how to convert it to a normal integer.
+*/
+
 int main() {
-    
+    // This what I'm going to do in class tomorrow
 }
