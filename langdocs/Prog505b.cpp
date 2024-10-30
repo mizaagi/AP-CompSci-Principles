68d564c379a411ab56d70938e9e75171b874f202
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-10-30 16:16:18 +0000

    10/30/2024

--- langdocs/Prog505b.cpp
+++ langdocs/Prog505b.cpp
@@ -6,13 +6,18 @@ using namespace std;
 
 string str(int i) {return to_string(i);}
 string strf(float i) {return to_string(i);}
+int As;
+int Bs;
+int Cs;
+int Ds;
+int Fs;
 
 string getGrade(double percent) {
-    if (percent >= 90) return "A";
-    else if (percent >= 80) return "B";
-    else if (percent >= 70) return "C";
-    else if (percent >= 60) return "D";
-    else return "F";
+    if (percent >= 90) {As++; return "A";}
+    else if (percent >= 80) {Bs++; return "B";}
+    else if (percent >= 70) {Cs++; return "C";}
+    else if (percent >= 60) {Ds++; return "D";}
+    else {Fs++; return "F";}
 }
 
 string format(string f, string l, int s1, int s2, int s3, int s4, int s5) {
@@ -22,9 +27,9 @@ string format(string f, string l, int s1, int s2, int s3, int s4, int s5) {
 
 int main() {
     double t1, t2, t3, t4, t5;
-    int tCount;
+    float tCount;
     string first, last;
-    int s1, s2, s3, s4, s5, aS, bS, cS, dS, fS;
+    int s1, s2, s3, s4, s5;
     ifstream f("prog505b.dat");
     if (f.is_open()) {
         while (f >> first >> last >> s1 >> s2 >> s3 >> s4 >> s5) {
@@ -32,8 +37,11 @@ int main() {
             t1 += s1; t2 += s2; t3 += s3; t4 += s4; t5 += s5;
             tCount++;
         }
-        for (int i = 0; i < tCount; i++) {
-            
-        }
+        cout << "Test 1\t" << t1/tCount << "\tA " << As << endl;
+        cout << "Test 2\t" << t2/tCount << "\tB " << Bs << endl;
+        cout << "Test 3\t" << t3/tCount << "\tC " << Cs << endl;
+        cout << "Test 4\t" << t4/tCount << "\tD " << Ds << endl;
+        cout << "Test 5\t" << t5/tCount << "\tF " << Fs << endl;
+
     } f.close();
 }
