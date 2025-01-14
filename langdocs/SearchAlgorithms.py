e3823e90f7eb9678d99ef95fb75941060c7aa60a
mizaagi <126592834+mizaagi@users.noreply.github.com>
2025-01-14 15:57:18 +0000

    1/14/2025 - 408b

--- langdocs/SearchAlgorithms.py
+++ langdocs/SearchAlgorithms.py
@@ -3,3 +3,15 @@ def linear_search(list, target):
         if list[i] == target:
             return i
     
+def binary_search(list, target):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = int(low + (high - low)/2)
+        if list[mid] == target:
+            return mid
+        elif list[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
--- langdocs/prog408b.py
+++ langdocs/prog408b.py
@@ -1,9 +1,37 @@
+import time
+import SearchAlgorithms as sa
+
 def main():
     try:
         data = []
-        with open("prog408a.txt", "r") as f:
+        with open("langdocs/prog408b.dat", "r") as f:
             data = [int(line) for line in f]
-        # Your code here
+        target = int(input("Enter the number to search for: "))
+        start = time.perf_counter()
+        index = sa.linear_search(data, target)
+        end = time.perf_counter()
+        if index:
+            print("Linear Search (Unsorted): Number found at index ", index)
+        else:
+            print("Linear Search (Unsorted): Number not found")
+        print(f"Search time: {end-start:.8f}")
+        data.sort()
+        start = time.perf_counter()
+        index = sa.linear_search(data, target)
+        end = time.perf_counter()
+        if index:
+            print("Linear Search (Sorted): Number found at index ", index)
+        else:
+            print("Linear Search (Sorted): Number not found")
+        print(f"Search time: {end-start:.8f}")
+        start = time.perf_counter()
+        index = sa.binary_search(data, target)
+        end = time.perf_counter()
+        if index:
+            print("Binary Search (Sorted): Number found at index ", index)
+        else:
+            print("Binary Search (Sorted): Number not found")
+        print(f"Search time: {end-start:.8f}")
         ...
     except Exception as e:
         print("Error: ", e)
